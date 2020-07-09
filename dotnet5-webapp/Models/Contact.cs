a6e6a41a34cbf0e79cc216763c40037fc7d146e4
Patrick <patrick@godknows.de>
2020-07-09 17:33:15 +0200

    Your First Model

---
+++ dotnet5-webapp/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet5_webapp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsDeleted { get; set; } = false;
+    }
+}
