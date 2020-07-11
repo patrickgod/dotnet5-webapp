22b413fb03c6bfc7bd0c3ac6584e26d28be191fd
Patrick <patrick@godknows.de>
2020-07-11 11:13:19 +0200

    Soft Delete

--- dotnet5-webapp/Controllers/ContactsController.cs
+++ dotnet5-webapp/Controllers/ContactsController.cs
@@ -25,7 +25,7 @@ namespace dotnet5_webapp.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
         {
-            return await _context.Contacts.ToListAsync();
+            return await _context.Contacts.Where(c => c.IsDeleted == false).ToListAsync();
         }
 
         // GET: api/Contacts/5
@@ -84,7 +84,8 @@ namespace dotnet5_webapp.Controllers
                 return NotFound();
             }
 
-            _context.Contacts.Remove(contact);
+            contact.IsDeleted = true;
+
             await _context.SaveChangesAsync();
 
             return Ok(_context.Contacts);
