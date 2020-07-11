ea25aad573a9ec7c85d4a0562159b61aeea6730b
Patrick <patrick@godknows.de>
2020-07-11 08:35:07 +0200

    Implementing the DataContext

---
+++ dotnet5-webapp/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using dotnet5_webapp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet5_webapp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
